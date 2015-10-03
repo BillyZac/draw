1268b2118424aeb22b73a639a6318d73e28d8371
Zac Smith <billyzacsmith@gmail.com>
2015-10-03 11:57:04 -0600

    Draw circle, but only on the grid.

--- js/app.js
+++ js/app.js
@@ -35,18 +35,18 @@ function changeColor() {
   $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
 }
 
-//When color sliders change
-$("input[type=range]").on("input", changeColor);
+// //When color sliders change
+// $("input[type=range]").on("input", changeColor);
 
-//when "Add color" is pressed
-$("#addNewColor").click(function(){
-  //append the color to the controls <ul>
-  var $newColor = $("<li></li>");
-  $newColor.css("background-color", $("#newColor").css("background-color")); 
-  $(".controls ul").append($newColor);
-  //Select the new color
-  $newColor.click();
-});
+// //when "Add color" is pressed
+// $("#addNewColor").click(function(){
+//   //append the color to the controls <ul>
+//   var $newColor = $("<li></li>");
+//   $newColor.css("background-color", $("#newColor").css("background-color")); 
+//   $(".controls ul").append($newColor);
+//   //Select the new color
+//   $newColor.click();
+// });
   
 //On mouse events on the canvas
 $canvas.mousedown(function(e){
@@ -62,10 +62,29 @@ $canvas.mousedown(function(e){
     context.stroke();
     lastEvent = e;
   }
+
+  // Draw a circle where the mouse is clicked
+  if (mouseDown) {
+    console.log(e.offsetX);
+    drawCircle(e.offsetX, e.offsetY);
+  }
+
+
 }).mouseup(function(){
   mouseDown = false;
 }).mouseleave(function(){
   $canvas.mouseup();
 });
 
+
+function drawCircle(x, y) {
+  context.beginPath();
+  x = (Math.round(x / 20)) * 20;
+  y = (Math.round(y / 20)) * 20;
+
+  context.ellipse(x, y, 10, 10, 50 * Math.PI/180, 0, 2 * Math.PI);
+  context.fill();
+}
+
+drawCircle(50, 50);
 
